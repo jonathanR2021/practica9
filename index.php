d06ce8f58552ccbc05215a1af882e50b75a7e2d7
jonathanR2021 <jonathan.ramos20@itca.edu.sv>
2021-05-17 10:21:04 -0600

    agregue el menu de navegacion y los integrantes

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+    <link rel="stylesheet" href="icon/css/fontello.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/cuerpo.css">
+</head>
+<body>
+    <main>
+        <div class="content-all">
+            <header></header>
+            <input type="checkbox" id="check">
+            <label for="check" class="icon-menu">MENÚ</label>
+            <h2>ITCA 2021</h2>
+            <nav class="menu">
+                <ul>
+                    <li> <a href="index.php">Inicio</a></li>
+                    <li> <a href="Ejercicio1.php"> Ejercicio1</a></li>
+                    <li> <a href="Ejercicio2.php">Ejercicio2</a> </li>
+                </ul>
+            </nav>
+            <article>
+                <h1>PRACTICA 9</h1>
+                <p class="parrafo1">En esta pagina web podremos encontrar los 2 ejercicios restantes de la practica 9</p>
+                <br>
+                <h3>INTEGRANTES</h3>
+                <p class="parrafo1">Jose Mauricio Sandoval Linares</p>
+                <p class="parrafo1">Roger Adonaldo Moreno Granados</p>
+                <p class="parrafo1">Elba Roxana Mendez Medina</p>
+                <p class="parrafo1">Jonathan Vladimir Ramos Ceritos</p>
+                <img src="image/ph2.png">
+            </article>
+        </div>
+    </main>
+</body>
+
+</html>
