e52400ddef701313a791ef0f9c5a249b4ecb8f63
jonathanR2021 <jonathan.ramos20@itca.edu.sv>
2021-05-17 12:43:25 -0600

    agregamos el archivo testeo.php Edwin Morales

---
+++ testeo.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class testeo extends TestCase
+{
+	public function testradio()
+	{
+		$calr= new radio();
+		$this->assert(-> area()
+	);
+		
+	}
+}
+
+?>
