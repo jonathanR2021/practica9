45c64226e7269d43393051554b3beb58ba784255
EdwinPerez1 <83798675+EdwinPerez1@users.noreply.github.com>
2021-05-19 23:07:38 -0600

    Version final

--- testeo.php
+++ testeo.php
@@ -1,14 +1,22 @@
 <?php
 
+require_once("funsiones.php");
+
 use PHPUnit\Framework\TestCase;
 
 final class testeo extends TestCase
 {
 	public function testradio()
 	{
-		$calr= new radio();
-		$this->assert(-> area()
-	);
+		$obj= new clsf();
+		$this->assertEquals(12.57,$obj->rad(2));
+		
+	}
+	
+	public function testpromedio()
+	{
+		$obj= new clsf();
+		$this->assertEquals(1.28,$obj->promedio(7,0.183333));
 		
 	}
 }
